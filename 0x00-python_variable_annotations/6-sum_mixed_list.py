cdf91d3f23f4fd49a03ee3539a6acde201af0aca
ihavemadefire <jacobmeide@gmail.com>
2021-03-15 14:15:49 -0500

    Update docstrings

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-'''takes in a list of floats and sums them'''
+'''takes in a mixed list of floats and ints sums them'''
 from typing import Mapping
 
 
 def sum_mixed_list(mxd_lst: Mapping[int, float]) -> float:
-    '''takes in a list of floats and sums them'''
+    '''takes in a mixed list of floats and ints returns sums'''
     return sum(mxd_lst)
