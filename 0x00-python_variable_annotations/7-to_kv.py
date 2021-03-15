90df7e0f27b3466665d8c33c8fe2ca1defcb35b1
ihavemadefire <jacobmeide@gmail.com>
2021-03-15 15:11:09 -0500

    Initial commit

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''takes in a mixed of key value pairs and returns it as a tuple'''
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''takes in a mixed of key value pairs and returns it as a tuple'''
+    return (k, v)
