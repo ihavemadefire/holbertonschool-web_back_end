2bd83f41a1e7df621f0119591b4e4efef7086936
ihavemadefire <jacobmeide@gmail.com>
2021-03-15 13:52:02 -0500

    Initial commit

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''takes in a list of floats and sums them'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''takes in a list of floats and sums them'''
+    return sum(input_list)
