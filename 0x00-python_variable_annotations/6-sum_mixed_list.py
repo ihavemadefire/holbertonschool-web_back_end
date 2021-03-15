50867c3c8b63193793181d3fd3bfb10eb320cdf5
ihavemadefire <jacobmeide@gmail.com>
2021-03-15 14:34:06 -0500

    Changed duck type

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''takes in a mixed list of floats and ints sums them'''
-from typing import Mapping
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Mapping[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''takes in a mixed list of floats and ints returns sums'''
     return sum(mxd_lst)
