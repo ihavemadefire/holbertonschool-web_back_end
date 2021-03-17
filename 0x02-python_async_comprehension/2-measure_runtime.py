272ce99cf5df294365cb73e65ec4e6435475ece2
ihavemadefire <jacobmeide@gmail.com>
2021-03-17 16:03:55 -0500

    adjust style

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     '''Basic Async measure '''
     t1 = time.time()
