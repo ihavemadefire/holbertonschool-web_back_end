0317788ce7f399e198fd0d37a14f9751bf19b250
ihavemadefire <jacobmeide@gmail.com>
2021-03-17 14:30:47 -0500

    initial attemp

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Basic Async Generator'''
+import random
+import asyncio
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    '''Basic Async Generator'''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
