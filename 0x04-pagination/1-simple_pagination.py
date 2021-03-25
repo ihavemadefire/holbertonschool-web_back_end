23675ab30c16160883b4047b9f81fc783a3dd6bd
ihavemadefire <jacobmeide@gmail.com>
2021-03-24 20:22:14 -0500

    Initial attempt

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+This module creates a helper function
+"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    '''simple helper function'''
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+            test page length
+            open dataset file
+            find indices
+            return their value
+        '''
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        self.dataset()
+        a = index_range(page, page_size)[0]
+        b = index_range(page, page_size)[1]
+        if type(self.__dataset) is not None:
+            return self.__dataset[a:b]
+        else:
+            return []
