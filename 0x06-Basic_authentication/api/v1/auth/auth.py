44945036280be2761372a570d4f13401af321263
ihavemadefire <jacobmeide@gmail.com>
2021-03-30 15:05:25 -0500

    initial attempt for auth functions

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''This module contains the auth class'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''This funtion requires auth for endpoints'''
+        if path is None or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path = path + '/'
+        if path not in excluded_paths:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''This function requires auth for enpoints'''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''This function returns the current user'''
+        return None
