bb5c68e0c0307b4412aa418d4507412a4d7ca328
ihavemadefire <jacobmeide@gmail.com>
2021-03-29 20:54:45 -0500

    Add class

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 '''This is the module that constains the filter datum function'''
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -13,3 +14,22 @@ def filter_datum(fields: List[str],
         regex = field + f'[^{separator}]*'
         message = re.sub(regex, f"{field}={redaction}", message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Formating function'''
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record),
+            self.SEPARATOR)
