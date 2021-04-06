f2f5ccbdade0651c017f1a42d05af6651551e557
ihavemadefire <jacobmeide@gmail.com>
2021-04-06 11:40:09 -0500

    initial commit

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 '''This module contains the SessionAuth class'''
 from .auth import Auth
+import uuid
 
 class SessionAuth(Auth):
     '''The session authorization class'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''This is the create session function'''
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        self.id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[self.id] = user_id
+        return self.id
