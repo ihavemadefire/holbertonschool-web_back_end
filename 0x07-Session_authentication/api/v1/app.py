b3073522d9d30d2530cc0fe8bd811d8a69a8c384
ihavemadefire <jacobmeide@gmail.com>
2021-04-06 13:07:23 -0500

    Initial commit

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -26,7 +26,8 @@ elif auth_type == 'session_auth':
     auth = SessionAuth()
 excluded_paths = ['/api/v1/status/',
                   '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/']
+                  '/api/v1/forbidden/',
+                  '/api/v1/auth_session/login/']
 
 
 @app.errorhandler(401)
@@ -54,10 +55,14 @@ def not_found(error) -> str:
 def before_request_func() -> Dict:
     '''Requires auth before request'''
     if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None and auth.session_cookie(
+            request) is None:
+            abort(401)
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+        
         request.current_user = auth.current_user(request)
 
 
