ce701de2522400101a8b0c4af7cdceed5262f4a6
ihavemadefire <jacobmeide@gmail.com>
2021-04-07 19:57:01 -0500

    removed exraneous comment

--- 0x07-Session_authentication/models/user_session.py
+++ 0x07-Session_authentication/models/user_session.py
@@ -9,8 +9,6 @@ class UserSession(Base):
     """
 
     def __init__(self, *args: list, **kwargs: dict):
-        """ Initialize a User instance
-        """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
