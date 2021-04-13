566b025364099fdc43a32d1661268be0a3f13dc3
ihavemadefire <jacobmeide@gmail.com>
2021-04-13 14:18:47 -0500

    Remove ptentially problematice imports

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -3,7 +3,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from user import Base
 from user import User
 
