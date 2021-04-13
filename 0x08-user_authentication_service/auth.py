164d58690b2d19eaa2f40ff07483e316b0a2846a
ihavemadefire <jacobmeide@gmail.com>
2021-04-13 17:16:07 -0500

    change return type

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 '''This module contains the book of amun ra'''
-from typing import ByteString
 import bcrypt
 
 
-def _hash_password(password: str) -> ByteString:
+def _hash_password(password: str) -> str:
     '''Returns a hashed user password'''
     if password and isinstance(password, str):
         salt = bcrypt.gensalt()
