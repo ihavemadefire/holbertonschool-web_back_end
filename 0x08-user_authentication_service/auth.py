24163e320be35bc350cb6900522c15d8213d63a5
ihavemadefire <jacobmeide@gmail.com>
2021-04-13 17:14:35 -0500

    add instance check

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -6,7 +6,7 @@ import bcrypt
 
 def _hash_password(password: str) -> ByteString:
     '''Returns a hashed user password'''
-    if password:
+    if password and isinstance(password, str):
         salt = bcrypt.gensalt()
         return bcrypt.hashpw(str.encode(password), salt)
     return None
