3d12c5cb43560c6741f506aef2788b7daf2d8c53
ihavemadefire <jacobmeide@gmail.com>
2021-04-13 20:09:07 -0500

    correct formatting

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -6,10 +6,12 @@ from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     '''basic route'''
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
@@ -22,6 +24,7 @@ def users():
                             "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
