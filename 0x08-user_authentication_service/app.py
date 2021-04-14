3e104bbc41bfa5a0fae0de5df928b31f4fdbdcd7
ihavemadefire <jacobmeide@gmail.com>
2021-04-13 22:02:40 -0500

    initial attempt

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''This modu'''
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+from flask.helpers import make_response
 app = Flask(__name__)
 AUTH = Auth()
 
@@ -24,6 +25,20 @@ def users():
                             "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''login route'''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        session = AUTH.create_session(email)
+        r = make_response(jsonify({"email": email, "message": "logged in"}))
+        r.set_cookie("session_id", session)
+        return session
 
 
 if __name__ == "__main__":
