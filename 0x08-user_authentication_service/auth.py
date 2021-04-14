eeffab9e29d7bfcdc56c7b898bf797447e42518a
ihavemadefire <jacobmeide@gmail.com>
2021-04-13 22:42:44 -0500

    add none return

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -64,4 +64,7 @@ class Auth:
     def get_user_from_session_id(self, session_id: int) -> Union[User, None]:
         '''Gets user from session ID'''
         u = self._db.find_user_by(session_id=session_id)
-        return u
+        if u:
+            return u
+        else:
+            return None
