312bd6e6eb8f06cbf208d265f275a406b0620cae
ihavemadefire <jacobmeide@gmail.com>
2021-04-19 17:30:03 -0500

    change get_jason target

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 '''This module tests the github org functions'''
 from parameterized import parameterized
 from client import GithubOrgClient
-from utils import get_json
+from client import get_json
 import unittest
 from unittest.mock import patch
 
@@ -13,10 +13,10 @@ class TestGithubOrgClient(unittest.TestCase):
         'google',
         'abc'
     ])
-    @patch('get_json', return_value={'key': 'value'})
+    @patch('client.get_json', return_value={'key': 'value'})
     def test_org(self, org, mock_get_json):
         '''This tests if a mocked up return does some things.'''
         test = GithubOrgClient(org)
         url = "https://api.github.com/orgs/" + org
-        self.assertEqual(test_obj.org, {'key': 'value'})
+        self.assertEqual(test.org, {'key': 'value'})
         mock_get_json.assert_called_once_with(url)
