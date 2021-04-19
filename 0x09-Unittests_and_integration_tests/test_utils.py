e1df0dfa2a34591e8560f488a5a42a288e5ed784
ihavemadefire <jacobmeide@gmail.com>
2021-04-19 14:18:51 -0500

    fully refeact params

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -17,9 +17,9 @@ from utils import access_nested_map, get_json, memoize
 class TestAccessNestedMap(unittest.TestCase):
     '''This class tests mapping methods'''
     @parameterized.expand([
-        ({"a": 1}, ["a", ], 1),
-        ({"a": {"b": 2}}, ["a", ], {"b": 2}),
-        ({"a": {"b": 2}}, ["a", "b"], 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence, exp: int) -> Any:
@@ -27,8 +27,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), exp)
 
     @parameterized.expand([
-        ({}, ["a", ]),
-        ({"a": 1}, ["a", "b"])
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map: Mapping,
                                          path: Sequence) -> Any:
