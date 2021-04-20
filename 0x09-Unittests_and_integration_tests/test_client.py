b0d97e560fba8e0af01f8dd42df6f1e2a4350e7f
ihavemadefire <jacobmeide@gmail.com>
2021-04-19 19:41:27 -0500

    Initial attempt for number 6

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -29,3 +29,20 @@ class TestGithubOrgClient(unittest.TestCase):
             test = GithubOrgClient('ThisIsNotARealOrg')
             self.assertEqual(test._public_repos_url,
                              "http://thisisnotreal.com")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        '''Tests the public repos function'''
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mockItUp:
+            fakeload = {'Date': '11/22/2013',
+                        'name': 'zaphod Beeblbrox',
+                        'repos_url': 'http://thisisnotreal.com'}
+            test = GithubOrgClient('NotReal')
+            mock_get_json.return_value = fakeload
+            org_name = test.org
+            mockItUp.return_value = org_name.get('repos_url')
+            self.assertEqual(test._public_repos_url,
+                             "http://thisisnotreal.com")
+            mock_get_json.assert_called_once()
+            mockItUp.assert_called_once()
