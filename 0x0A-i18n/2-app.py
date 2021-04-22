cd8e47249980cf04b0e2fa9b0ee1ed5e34a34c33
ihavemadefire <jacobmeide@gmail.com>
2021-04-22 13:02:35 -0500

    add locale selector

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''This module is the main flask application'''
+from flask import Flask, render_template. request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    '''this is the config class'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello_world():
+    '''The only route'''
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    return request.accept_languages.best_match(['fr', 'en'])
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run()
