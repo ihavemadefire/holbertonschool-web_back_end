a3b7e5c8beb39e5165b79c062d2dbd15edc5d44e
ihavemadefire <jacobmeide@gmail.com>
2021-04-22 18:53:33 -0500

    fix formatting

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -26,7 +26,7 @@ def hello_world():
 @babel.localeselector
 def get_locale():
     '''This gets the locale'''
-    locale =  request.args.get('locale')
+    locale = request.args.get('locale')
     if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
