21364e5393c846ee060f6f44f4371e8ea4192c37
ihavemadefire <jacobmeide@gmail.com>
2021-04-29 09:59:12 -0500

    Change name add newline

---
+++ 0x0B_redis_basic/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Main file """
+get_page = __import__('web').get_page
+
+
+get_page('http://www.google.com')
--- 0x0C-MySQL_Advanced/9-index_name_score.sql
+++ 0x0C-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Creates index on name and score
 -- More indexing
-CREATE INDEX idx_name_first ON names(name(1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score);
