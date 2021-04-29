2ad689bdbf5f80f2103751fadc4ee43af3af520f
ihavemadefire <jacobmeide@gmail.com>
2021-04-29 12:32:54 -0500

    remove typo

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,3 @@
 -- This creates a view
 -- Regarding tables and meetings
 CREATE VIEW need_meeting AS SELECT name FROM students WHERE (score < 80) AND (DATE_SUB(now(), INTERVAL 1 MONTH) OR ISNULL(last_meeting));
-q   
