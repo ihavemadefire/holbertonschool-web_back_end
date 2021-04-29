ef318b316457987ea168a7bb604511d9607aa1a9
ihavemadefire <jacobmeide@gmail.com>
2021-04-29 12:32:10 -0500

    initial attempt

---
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,4 @@
+-- This creates a view
+-- Regarding tables and meetings
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE (score < 80) AND (DATE_SUB(now(), INTERVAL 1 MONTH) OR ISNULL(last_meeting));
+q   
