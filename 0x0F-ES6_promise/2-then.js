14972dd93855245848fb163150de5594d342b130
ihavemadefire <jacobmeide@gmail.com>
2021-05-11 19:07:57 -0500

    Fixed linter and added fixed prototype

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
-export default function getResponseFromAPI(success) {
+export default function getResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
-    body: 'success'
+    body: 'success',
   })).catch(() => Error())
     .finally(() => {
       console.log('Got a response from the API');
