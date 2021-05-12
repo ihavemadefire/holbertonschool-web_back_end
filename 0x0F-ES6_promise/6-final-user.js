f99683a6d612d606acd18535697b347eb3022fd8
ihavemadefire <jacobmeide@gmail.com>
2021-05-11 20:14:23 -0500

    That was not so simple.

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,5 +2,25 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(
+      (res) => {
+        const ret = [];
+        for (const i of res) {
+          if (i.status === 'fulfilled') {
+            ret.push({
+              status: i.status,
+              value: i.value,
+            });
+          } else {
+            ret.push({
+              status: i.status,
+              value: `Error: ${i.reason.message}`,
+            });
+          }
+        }
+
+        return ret;
+      },
+    );
 }
