a55766fd6e713f12805160cb8321669f40ae1a63
ihavemadefire <jacobmeide@gmail.com>
2021-05-11 19:26:13 -0500

    change to f string literals

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -5,8 +5,6 @@ export default function handleProfileSignup() {
     uploadPhoto(),
     createUser(),
   ]).then((values) => {
-    console.log(values[0].body,
-      values[1].firstName,
-      values[1].lastName);
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   }).catch(() => console.log('Signup system offline'));
 }
