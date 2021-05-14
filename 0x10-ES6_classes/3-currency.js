accdf39bb5d0f0a7dbaab714d16beb55e1050cc9
ihavemadefire <jacobmeide@gmail.com>
2021-05-13 21:50:33 -0500

    Initial attempt

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,1 +1,30 @@
-#!/usr/bin/node
+export default class Currency {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof code !== 'string') throw TypeError('Code must be a string');
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(anotherName) {
+    if (typeof anotherName !== 'string') throw TypeError('Name must be a string');
+    this._name = anotherName;
+  }
+
+  set code(anotherCode) {
+    if (typeof anotherCode !== 'string') throw TypeError('Code must be a string');
+    this._name = anotherCode;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
