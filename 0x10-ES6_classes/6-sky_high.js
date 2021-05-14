362c2a94dd8546ceecdb2dca6c9371a38c11791f
ihavemadefire <jacobmeide@gmail.com>
2021-05-13 22:39:39 -0500

    Initial attempt

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -1,21 +1,20 @@
-/* eslint-disable */
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-	constructor(sqft, floors) {
-		super(sqft);
-		this._floors = floors;
-	}
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-	get floors() {
-		return this._floors;
-	}
+  get floors() {
+    return this._floors;
+  }
 
-	set floors(moreFloors) {
-		this._floors = newFloors;
-	}
+  set floors(moreFloors) {
+    this._floors = moreFloors;
+  }
 
-	evacuationWarningMessage() {
-		return `Evacuate slowly the ${this._floors} floors`
-	}
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
