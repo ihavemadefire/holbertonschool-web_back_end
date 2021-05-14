64d057549c8643175b7e130ed9e67b2113cbbcb2
ihavemadefire <jacobmeide@gmail.com>
2021-05-13 22:49:21 -0500

    Initial attempt

--- 0x10-ES6_classes/8-hbtn_class.js
+++ 0x10-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+}
