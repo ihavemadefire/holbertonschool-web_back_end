46a289359822fcea9ac6526d635dfde40902d11f
ihavemadefire <jacobmeide@gmail.com>
2021-05-13 20:49:13 -0500

    reversed refactoring and fixed obvious typo

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -5,29 +5,24 @@ export default class HolbertonCourse {
     if (!Array.isArray(students)) throw TypeError('Students must be an array');
     this._name = name;
     this._length = length;
-    this._student = students;
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(anotherName) {
-    if (typeof anotherName !== 'string') throw TypeError('Name must be a string');
-    this._name = anotherName;
-  }
-
   get length() {
     return this._length;
+  }
+
+  get students() {
+    return this._students;
   }
 
   set length(anotherLength) {
     if (typeof anotherLength !== 'number') throw TypeError('Length must be a number');
     this._length = anotherLength;
-  }
-
-  get students() {
-    return this._students;
   }
 
   set students(moreStudents) {
