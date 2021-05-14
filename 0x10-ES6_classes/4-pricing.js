67edb27ff22fba1353d791b1911bab553327edf2
ihavemadefire <jacobmeide@gmail.com>
2021-05-13 22:15:26 -0500

    Initial attempt

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,1 +1,33 @@
-#!/usr/bin/node
+/* eslint-disable */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+		this._amount = amount;
+		this._currency = currency;
+	}
+
+	get amount() {
+		return this._amount;
+	}
+
+	get currency() {
+		return this._currency;
+	}
+
+	set amount(newAmount) {
+		this._amount = newAmount;
+	}
+
+	set currency(newCurrency) {
+		this._currency = newCurrency;
+	}
+
+	displayFullPrice() {
+		return `${this._amount} ${this._currency._name} (${this._currency._code})`
+	}
+
+	static convertPrice(amount, conversionRate) {
+		return amount * conversionRate;
+	}
+}
