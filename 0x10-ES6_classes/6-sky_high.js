9859411b61a3334d136128d50326febdb20024e2
ihavemadefire <jacobmeide@gmail.com>
2021-05-13 22:29:33 -0500

    Initial attempt

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+	constructor(sqft, floors) {
+		super(sqft);
+		this._floors = floors;
+	}
+
+	get floors() {
+		return this._floors;
+	}
+
+	set floors(moreFloors) {
+		this._floors = newFloors;
+	}
+
+	evacuationWarningMessage() {
+		return `Evacuate slowly the ${this._floors} floors`
+	}
+}
