b9134c3861d1a4e6686fb27185c3de8aa12bf0eb
ihavemadefire <jacobmeide@gmail.com>
2021-05-17 11:40:45 -0500

    Initial attempt

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  view.setInt8(position, value);
+  return view;
+}
