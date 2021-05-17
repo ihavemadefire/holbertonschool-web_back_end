d1aecba5f962f7c51d014da6015e1dbdf79a6027
ihavemadefire <jacobmeide@gmail.com>
2021-05-17 12:22:36 -0500

    change function to startsWith()

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (typeof startString === 'string' && startString) {
     const ret = [];
     for (const i of set) {
-      if (i.indexOf(startString) === 0) {
+      if (i.startsWith(startString)) {
         ret.push(i.slice(startString.length));
       }
     }
