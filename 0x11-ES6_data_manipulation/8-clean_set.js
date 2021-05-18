744aec1e9bfbd88b328750d9afbe09bf4bfc160f
ihavemadefire <jacobmeide@gmail.com>
2021-05-18 18:57:43 -0500

    Change to index of

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (typeof startString === 'string' && startString !== '') {
     const ret = [];
     for (const i of set) {
-      if (i.startsWith(startString)) {
+      if (i.indexOf(startString) === 0) {
         ret.push(i.slice(startString.length));
       }
     }
