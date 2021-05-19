b5e3d6444f0d57ef2d250e0eb012ba2460a13cf4
ihavemadefire <jacobmeide@gmail.com>
2021-05-18 19:31:04 -0500

    Add extra conditional

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (typeof startString === 'string' && startString !== '') {
     const ret = [];
     for (const i of set) {
-      if (i.indexOf(startString) === 0) {
+      if (typeof i === 'string' && i.indexOf(startString) === 0) {
         ret.push(i.slice(startString.length));
       }
     }
