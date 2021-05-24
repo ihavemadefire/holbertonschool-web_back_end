99d6578c17b16d66ad15086e6baf5c7bcb0b6ce5
ihavemadefire <jacobmeide@gmail.com>
2021-05-24 14:35:35 -0500

    Initial commit

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
+  }
+  const payload = fs.readFileSync(path, 'utf8');
+  const students = payload.split('\n')
+    .map((student) => student.split(','))
+    .filter((student) => student[0] !== 'firstname' && student.length === 4);
+  const allCount = students.length;
+  const csCount = students.filter((student) => student[3] === 'CS').length;
+  const sweCount = students.filter((student) => student[3] === 'SWE').length;
+  const csList = students.filter((student) => student[3] === 'CS')
+    .map((student) => student[0]);
+  const sweList = students.filter((student) => student[3] === 'SWE')
+    .map((student) => student[0]);
+  console.log(`Number of students: ${allCount}`);
+  console.log(`Number of students in CS: ${csCount}. List: ${csList}`);
+  console.log(`Number of students in SWE: ${sweCount}. List: ${sweList}`);
+}
+
+module.exports = countStudents;
