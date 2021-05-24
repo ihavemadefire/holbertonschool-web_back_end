b7caccdd78703e6c34c01f29195543ea6850367b
ihavemadefire <jacobmeide@gmail.com>
2021-05-24 11:06:22 -0500

    fix output string

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   if (data !== null) {
-    process.stdout.write(`Your name is ${data.toString()}`);
+    process.stdout.write(`Your name is: ${data.toString()}`);
   }
 });
 process.stdin.on('end', () => {
