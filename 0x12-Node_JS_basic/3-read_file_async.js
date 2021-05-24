a0825634e56c560ed6392ed4488a16947c30a435
ihavemadefire <jacobmeide@gmail.com>
2021-05-24 15:36:53 -0500

    Initial attempt

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  let payload;
+  try {
+    payload = await fs.promises.readFile(path, 'utf8');
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+  const students = payload.split('\n')
+    .map((student) => student.split(','))
+    .filter((student) => student[0] !== 'firstname' && student.length === 4);
+  const allCount = students.length;
+  const csCount = students.filter((student) => student[3] === 'CS').length;
+  const sweCount = students.filter((student) => student[3] === 'SWE').length;
+  const csList = students.filter((student) => student[3] === 'CS')
+    .map((student) => student[0]).join(', ');
+  const sweList = students.filter((student) => student[3] === 'SWE')
+    .map((student) => student[0]).join(', ');
+  console.log(`Number of students: ${allCount}`);
+  console.log(`Number of students in CS: ${csCount}. List: ${csList}`);
+  console.log(`Number of students in SWE: ${sweCount}. List: ${sweList}`);
+}
+
+module.exports = countStudents;
