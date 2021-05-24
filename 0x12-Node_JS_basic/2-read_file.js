325d8721fdbb0ef14c58b146fd146c281fbd606f
ihavemadefire <jacobmeide@gmail.com>
2021-05-24 14:47:31 -0500

    Add join to list

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -12,9 +12,9 @@ function countStudents(path) {
   const csCount = students.filter((student) => student[3] === 'CS').length;
   const sweCount = students.filter((student) => student[3] === 'SWE').length;
   const csList = students.filter((student) => student[3] === 'CS')
-    .map((student) => student[0]);
+    .map((student) => student[0]).join(', ');
   const sweList = students.filter((student) => student[3] === 'SWE')
-    .map((student) => student[0]);
+    .map((student) => student[0]).join(', ');
   console.log(`Number of students: ${allCount}`);
   console.log(`Number of students in CS: ${csCount}. List: ${csList}`);
   console.log(`Number of students in SWE: ${sweCount}. List: ${sweList}`);
