acf761184e7514f348fda94034eef3fef95f5d02
ihavemadefire <jacobmeide@gmail.com>
2021-05-24 20:55:18 -0500

    add in poinlet plate from example

--- 0x12-Node_JS_basic/full_server/server.js
+++ 0x12-Node_JS_basic/full_server/server.js
@@ -8,6 +8,8 @@ app.use('/', routes);
 app.use('/students', routes);
 app.use('/students/:major', routes);
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Example app listening at http://localhost:1245');
+});
 
 export default app;
